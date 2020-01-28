6719d7309815932d4fd8c37a45cd4330d227971f
Ilya <agronat88@icloud.com>
2020-01-28 19:11:55 +0300

    added saveFunc

--- CoreDataApp/Controllers/ViewController.swift
+++ CoreDataApp/Controllers/ViewController.swift
@@ -21,8 +21,7 @@ class ViewController: UIViewController {
     @IBAction func addName(_ sender: Any) {
         var alert = UIAlertController(title: "New name", message: "Add a new name", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save", style: .default) { (action) in
-            let textField = alert.textFields?[0]
-            names.append(textField!.text!)
+            let textField = alert.textFields?.first
             self.tableView.reloadData()
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
@@ -37,12 +36,13 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = names[indexPath.row]
+        let person = people[indexPath.row]
+        cell.textLabel?.text = person.value(forKey: "name") as? String
         return cell
     }
     
--- CoreDataApp/HitModel/HitModel.swift
+++ CoreDataApp/HitModel/HitModel.swift
@@ -6,6 +6,27 @@
 //  Copyright © 2020 Li. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CoreData
 
-var names = [String]()
+var people = [NSManagedObject]()
+
+func save(name : String) {
+    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        else { return }
+    
+    let managedContext = appDelegate.persistentContainer.viewContext
+    
+    let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+    
+    let person = NSManagedObject(entity: entity, insertInto: managedContext)
+    
+    person.setValue(name, forKey: "name")
+    
+    do {
+        try managedContext.save()
+        people.append(person)
+    } catch let error as NSError {
+        print("Could not save. \(error), \(error.userInfo)")
+    }
+}
